413b6f45038f28792f82754f1a4f41bb8ebc77a0
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-07-08 18:38:59 -0300

    fix: add categoryContext and remove useLocation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import SignInPage from "../pages/SignInPage";
@@ -7,17 +7,24 @@ import HomePage from "../pages/HomePage";
 import CategoryPage from "../pages/CategoryPage";
 import ProductPage from "../pages/ProductPage";
 
+import CategoryContext from "../contexts/CategoryContext";
+
 function App() {
+  const [categoryName, setCategoryName] = useState("");
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/categories/:categoryId" element={<CategoryPage />} />
-        <Route path="/product/:productId" element={<ProductPage />} />
-      </Routes>
-    </BrowserRouter>
+    /* eslint-disable-next-line react/jsx-no-constructed-context-values */
+    <CategoryContext.Provider value={{ categoryName, setCategoryName }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/categories/:categoryId" element={<CategoryPage />} />
+          <Route path="/product/:productId" element={<ProductPage />} />
+        </Routes>
+      </BrowserRouter>
+    </CategoryContext.Provider>
   );
 }
 
